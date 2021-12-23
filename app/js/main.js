7abe56274fdc16e71f66d9e74c72b88784c69b2d
a.sivak <riziy_bes@mail.ru>
2021-12-23 11:39:17 +0300

    next

--- app/js/main.js
+++ app/js/main.js
@@ -92,6 +92,39 @@ window.addEventListener('load', function () {
             window.open(link, '_blank').focus();
         })
     })();
+
+    (function scroll() {
+        // Select all links with hashes
+        $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+                // On-page links
+                if (window.location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+                    && window.location.hostname === this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 300, function() {
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                                $target.focus();
+                            };
+                        });
+                    }
+                }
+            });
+    })();
 });
 
 function checkWidth() {
